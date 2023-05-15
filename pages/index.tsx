5ad8696e595294c72cd5a16bd06c17f97054a5d7
zcarian <mar.jed.00@gmail.com>
2023-05-15 16:47:13 +0200

    adding navigation bar and basic styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,7 @@
-import Link from "next/link";
-
 export default function Home() {
   return (
       <main>
-        <h1>Hello</h1>
-        <Link href="/add">Add New App</Link>  
-        <br/>
-        <Link href="/subscribed-apps">Subscribed Apps</Link>      
+        <h1>Hello</h1>     
       </main>
   );
 }
